74cc684d616a027af0b0190c2dbca80342559549
fritz52736-tech <fritz52736@gmail.com>
2026-01-17 00:08:44 -0600

    Update Program.cs
    Full code done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine(""); 
+        Console.WriteLine("Please enter your grade percent: "); 
+        string percent = Console.ReadLine(); 
+        int num = int.Parse(percent); 
+
+        if (num < 60)
+        {
+            Console.WriteLine("Your grade letter is F"); 
+        }
+        else if (num >= 90)
+        {
+            Console.WriteLine("Your grade letter is A");
+        }
+        else if (num >= 80)
+        {
+            Console.WriteLine("Your grade letter is B");
+        }
+        else if (num >= 70)
+        {
+            Console.WriteLine("Your grade letter is C");
+        }
+        else if (num >= 60)
+        {
+            Console.WriteLine("Your grade letter is D");
+        }
     }
 }
