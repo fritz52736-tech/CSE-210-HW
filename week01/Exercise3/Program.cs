c57e1f6aa776391173f32bbee1eff6a388d1f275
fritz52736-tech <fritz52736@gmail.com>
2026-01-17 19:19:51 -0600

    Update Program.cs
    Full code done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 21);
+
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine(""); 
+        Console.WriteLine("Okay! Let's play the number game! Are you ready? Start guessing! ");
+        string guess1 = Console.ReadLine(); 
+        int guess = int.Parse(guess1); 
+
+        while (guess != number)
+        {
+            if (guess > number)
+            {
+                Console.WriteLine("Lower!");
+                Console.WriteLine("Guess again! ");
+                guess1 = Console.ReadLine(); 
+                guess = int.Parse(guess1);
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher!"); 
+                Console.WriteLine("Guess again! "); 
+                guess1 = Console.ReadLine(); 
+                guess = int.Parse(guess1);
+            }
+            else if (number == guess)
+            {
+                Console.WriteLine("Correct! You win!!!! ");
+            }
+        }
+        Console.WriteLine("Correct! You win!!!! ");
     }
 }
